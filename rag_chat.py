966ae8bf1e0e918b01eae4a987bdaa7feb50a031
csaz9482 <luka.mirceta@student.uibk.ac.at>
2024-06-24 14:39:27 +0200

    Reformat rag_chat code.

--- rag_chat.py
+++ rag_chat.py
@@ -1,19 +1,11 @@
 import getpass
 import os
-#from constants import API_KEY
-#os.environ["GOOGLE_API_KEY"] = API_KEY
-
+from constants import API_KEY
 from langchain_google_vertexai import ChatVertexAI
-
-llm = ChatVertexAI(model="gemini-pro")
-from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-#import bs4
-from langchain.chains import LLMChain
 from langchain import hub
 from langchain_chroma import Chroma
-#from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -22,36 +14,37 @@ from langchain_community.document_loaders import UnstructuredMarkdownLoader
 from langchain_core.documents import Document
 from langchain.prompts import HumanMessagePromptTemplate 
 from langchain_core.prompts.prompt import PromptTemplate
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
 
-def rag_chat(question):
-    # In this project we are going to implement a QA App over Mardown data
-    # We will use pandoc to convert txt files to Markdown and the proceed with processing the data.
-    markdown_path = "./data.md"
+os.environ["GOOGLE_API_KEY"] = API_KEY
 
-    # We are going to use the Markdown loader to load the file into a editable format.
-    loader = UnstructuredMarkdownLoader(markdown_path)
+## We are going to use the gemini-pro model from Google for this project.
+llm = ChatVertexAI(model="gemini-pro")
+# In this project we are going to implement a QA App over Mardown data
+# We will use pandoc to convert txt files to Markdown and the proceed with processing the data.
+markdown_path = "./data.md"
+
+# We are going to use the Markdown loader to load the file into a editable format.
+loader = UnstructuredMarkdownLoader(markdown_path)
 
     
-    docs = loader.load()
+docs = loader.load()
 
-    # We are going to use the Google AI embedding to embed the data into a Vector Space.
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    
-    # We will use the recursive Character Splitter to split the data into chunks of 1000 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(docs)
-    
-    # We will use Chroma as a Vector store to store the chunks of data and embed them in there.
-    vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+print(len(docs[0].page_content))
 
-    # Retrieve and generate using the relevant snippets of the blog.
-    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
-
-    context = format_docs(retriever.invoke(question))
+# We are going to use the Google AI embedding to embed the data into a Vector Space.
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     
-    system_prompt = (
+# We will use the recursive Character Splitter to split the data into chunks of 1000 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, add_start_index=True)
+splits = text_splitter.split_documents(docs)
+    
+# We will use Chroma as a Vector store to store the chunks of data and embed them in there.
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+
+# Retrieve and generate using the relevant snippets of the blog.
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+    
+system_prompt = (
     "Vi ste asistent za zadatke odgovaranja na pitanja."
     "Koristite sledeće delove dobijenog konteksta da odgovorite na pitanje."
     "Ako ne znate odgovor, samo recite da ne znate."
@@ -60,6 +53,11 @@ def rag_chat(question):
     "\n\n"
     "{context}"
 )
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def rag_chat(question):
 
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -75,14 +73,9 @@ def rag_chat(question):
     | llm
     | StrOutputParser()
     )
-    response = chain.invoke(question)
-    print(response)
+
     output = ""
     for chunk in chain.stream(question):
         output += chunk
     return output
-    #print(retriever.invoke(question))
-
-#    return rag_chain.invoke(question)
-
 
